ebfe320458c6975bd84aa4c7208c101a6e5ed1b6
c.morais <c.morais1507@gmail.com>
2022-11-03 14:38:06 -0300

    :sparkles: feat: Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
 import { Home } from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <div>
+    <Header />
     <Home />
+    </div>
   )
 }
 
