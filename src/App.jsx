d8fa1991082f6fffb9b3532ce2b536808d9d5084
c.morais <c.morais1507@gmail.com>
2022-11-03 14:35:30 -0300

    :sparkles: feat: Add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import { Home } from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>Hello!</div>
+    <Home />
   )
 }
 
